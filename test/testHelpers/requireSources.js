8fbba779afea9dc504cdad012eb48fe0bfd688da
neuralsea-lab <emmett.miller7@outlook.com>
2021-02-21 18:02:35 +0000

    fixed coverage

--- test/testHelpers/requireSources.js
+++ test/testHelpers/requireSources.js
@@ -1,19 +1,16 @@
-import './../../src/js/index';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
+import { configure } from 'enzyme'
+import jsdom from 'jsdom'
 
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-import { configure } from 'enzyme';
-import jsdom from 'jsdom';
-
-//https://stackoverflow.com/questions/46896639/ensure-a-dom-environment-is-loaded-for-enzyme
 function setUpDomEnvironment() {
     const { JSDOM } = jsdom;
-    const dom = new JSDOM('<!doctype html><html><body></body></html>');
-    const { window, global } = dom;
+    const dom = new JSDOM('<!doctype html><html><body></body></html>', { url: 'http://localhost/' });
+    const { window } = dom;
 
     global.window = window;
     global.document = window.document;
     global.navigator = {
-        userAgent: 'node.js'
+        userAgent: 'node.js',
     };
     copyProps(window, global);
 }
@@ -26,5 +23,5 @@ function copyProps(src, target) {
 }
 
 setUpDomEnvironment();
-// eslint-disable-next-line prettier/prettier
-configure({ adapter: new Adapter() });
+
+configure({ adapter: new Adapter() })
